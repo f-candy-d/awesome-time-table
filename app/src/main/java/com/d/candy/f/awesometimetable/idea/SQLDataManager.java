83de9faf70fce55c8106f28ceb5820c470093579
f-candy-d <pagunouetamuramaro@gmail.com>
2017-07-14 12:07:09 +0900

    add new

---
+++ app/src/main/java/com/d/candy/f/awesometimetable/idea/SQLDataManager.java
@@ -0,0 +1,18 @@
+package com.d.candy.f.awesometimetable.idea;
+
+/**
+ * Created by daichi on 7/14/17.
+ */
+
+abstract public class SQLDataManager {
+
+    protected void insertEntity(Entity entity) {
+        String table_name = entity.getAffiliation();
+        // TODO: write insertion code here!
+    }
+
+    protected void deleteEntity(Entity entity) {
+        String table_name = entity.getAffiliation();
+        // TODO: write removing data code here
+    }
+}
