faf48b338ec4d9bb207513a97baccf5557304cd1
f-candy-d <pagunouetamuramaro@gmail.com>
2017-07-20 11:42:33 +0900

    adopt the liblary 'com.aurelhubert:ahbottomnavigation'

--- app/src/main/java/com/d/candy/f/awesometimetable/MainActivity.java
+++ app/src/main/java/com/d/candy/f/awesometimetable/MainActivity.java
@@ -1,10 +1,29 @@
+/**
+ * AHBottomNavigation library for Android
+ Copyright (c) 2017 Aurelien Hubert (http://github.com/aurelhubert).
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 package com.d.candy.f.awesometimetable;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.design.widget.NavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,9 +33,14 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+import com.aurelhubert.ahbottomnavigation.notification.AHNotification;
 import com.d.candy.f.awesometimetable.structure.EnrollingInfo;
+import com.d.candy.f.awesometimetable.structure.WeeklyTimeTable;
 import com.d.candy.f.awesometimetable.ui.SubjectDetailsActivity;
 import com.d.candy.f.awesometimetable.ui.WeeklyTimeTableFragment;
+import com.d.candy.f.awesometimetable.utils.DataStructureFactory;
 import com.d.candy.f.awesometimetable.utils.LogHelper;
 
 public class MainActivity extends AppCompatActivity
@@ -61,10 +85,13 @@ public class MainActivity extends AppCompatActivity
         // The following code make a bug that beyond me on orientation change...
 //        getSupportFragmentManager().beginTransaction()
 //                .add(R.id.fragment_container, new WeeklyTimeTableFragment()).commit();
-        WeeklyTimeTableFragment fragment = WeeklyTimeTableFragment.newInstance(null, null);
+        WeeklyTimeTableFragment fragment = WeeklyTimeTableFragment.newInstance();
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, fragment).commit();
         navigationView.setCheckedItem(R.id.nav_table1);
+
+        // Setup BottomNavigationBar
+        initBottomNavigaionBar();
     }
 
     @Override
@@ -131,6 +158,83 @@ public class MainActivity extends AppCompatActivity
         transaction.commit();
     }
 
+    private void initBottomNavigaionBar() {
+        AHBottomNavigation bottomNavigation = (AHBottomNavigation) findViewById(R.id.bottom_navigation_bar);
+
+        // Create items
+        AHBottomNavigationItem item1 = new AHBottomNavigationItem(
+                "tab1", R.drawable.ic_menu_camera, R.color.colorPrimary);
+        AHBottomNavigationItem item2 = new AHBottomNavigationItem(
+                "tab2", R.drawable.ic_menu_gallery, R.color.colorAccent);
+        AHBottomNavigationItem item3 = new AHBottomNavigationItem(
+                "tab3", R.drawable.ic_menu_slideshow, R.color.colorPrimaryDark);
+
+        // Add items
+        bottomNavigation.addItem(item1);
+        bottomNavigation.addItem(item2);
+        bottomNavigation.addItem(item3);
+
+// Set background color
+        bottomNavigation.setDefaultBackgroundColor(Color.parseColor("#FEFEFE"));
+
+// Disable the translation inside the CoordinatorLayout
+        bottomNavigation.setBehaviorTranslationEnabled(false);
+
+// Enable the translation of the FloatingActionButton
+//        bottomNavigation.manageFloatingActionButtonBehavior(floatingActionButton);
+
+// Change colors
+        bottomNavigation.setAccentColor(Color.parseColor("#F63D2B"));
+        bottomNavigation.setInactiveColor(Color.parseColor("#747474"));
+
+// Force to tint the drawable (useful for font with icon for example)
+        bottomNavigation.setForceTint(true);
+
+// Display color under navigation bar (API 21+)
+// Don't forget these lines in your style-v21
+// <item name="android:windowTranslucentNavigation">true</item>
+// <item name="android:fitsSystemWindows">true</item>
+//        bottomNavigation.setTranslucentNavigationEnabled(true);
+
+// Manage titles
+        bottomNavigation.setTitleState(AHBottomNavigation.TitleState.SHOW_WHEN_ACTIVE);
+        bottomNavigation.setTitleState(AHBottomNavigation.TitleState.ALWAYS_SHOW);
+        bottomNavigation.setTitleState(AHBottomNavigation.TitleState.ALWAYS_HIDE);
+
+// Use colored navigation with circle reveal effect
+        bottomNavigation.setColored(true);
+
+// Set current item programmatically
+        bottomNavigation.setCurrentItem(1);
+
+// Customize notification (title, background, typeface)
+        bottomNavigation.setNotificationBackgroundColor(Color.parseColor("#F63D2B"));
+
+// Add or remove notification for each item
+        bottomNavigation.setNotification("1", 2);
+// OR
+        AHNotification notification = new AHNotification.Builder()
+                .setText("1")
+                .setBackgroundColor(ContextCompat.getColor(this, R.color.cardview_light_background))
+                .setTextColor(ContextCompat.getColor(this, R.color.colorAccent))
+                .build();
+        bottomNavigation.setNotification(notification, 1);
+
+// Set listeners
+        bottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
+            @Override
+            public boolean onTabSelected(int position, boolean wasSelected) {
+                // Do something cool here...
+                return true;
+            }
+        });
+        bottomNavigation.setOnNavigationPositionListener(new AHBottomNavigation.OnNavigationPositionListener() {
+            @Override public void onPositionChange(int y) {
+                // Manage the new y position
+            }
+        });
+    }
+
     /**
      * Implementation of WeeklyTimeTableFragment.InteractionListener interface
      */
@@ -138,8 +242,11 @@ public class MainActivity extends AppCompatActivity
     public void onLaunchSubjectDetailsViewer(EnrollingInfo enrollingInfo) {
         Intent intent = new Intent(this, SubjectDetailsActivity.class);
         intent.putExtra(EXTRA_ENROLLING_INFO_ID, enrollingInfo.getID());
-        Log.d(TAG, "enrollingInfo.getID()=" + String.valueOf(enrollingInfo.getID()));
         startActivity(intent);
+    }
 
+    @Override
+    public WeeklyTimeTable onRequireTimeTableData() {
+        return DataStructureFactory.makeTimeTable(0);
     }
 }
