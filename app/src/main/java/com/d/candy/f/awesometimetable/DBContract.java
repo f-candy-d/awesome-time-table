1ff9802cea2b94fb4b2aa6a22938b46a19010352
f-candy-d <pagunouetamuramaro@gmail.com>
2017-07-21 20:51:44 +0900

    add new entity 'Notification'

--- app/src/main/java/com/d/candy/f/awesometimetable/DBContract.java
+++ app/src/main/java/com/d/candy/f/awesometimetable/DBContract.java
@@ -18,7 +18,8 @@ public class DBContract {
                 TeacherEntity.TABLE_NAME,
                 EnrollingInfoEntity.TABLE_NAME,
                 LocationEntity.TABLE_NAME,
-                AssignmentEntity.TABLE_NAME
+                AssignmentEntity.TABLE_NAME,
+                NotificationEntity.TABLE_NAME
         };
     }
 
@@ -35,23 +36,23 @@ public class DBContract {
         // (MIN_USABLE_ID - 1) is used for BLANK-Subject entity
         public static final int MIN_USABLE_ID = BLANK_SUBJECT_ID + 1;
         public static final String TABLE_NAME = "subject";
-        public static final String COLUMN_ID = "id";
-        public static final String COLUMN_NAME = "name";
-        public static final String COLUMN_ROOM_ID = "room_id";
-        public static final String CLOUMN_TEACHER_ID = "teacher_id";
-        public static final String COLUMN_LENGTH = "length";
+        public static final String COL_ID = "id";
+        public static final String COL_NAME = "name";
+        public static final String COL_ROOM_ID = "room_id";
+        public static final String COL_TEACHER_ID = "teacher_id";
+        public static final String COL_LENGTH = "length";
     }
 
     public static class TeacherEntity {
         private TeacherEntity() {}
 
         public static final String TABLE_NAME = "teacher";
-        public static final String COLUMN_ID = "id";
-        public static final String COLUMN_NAME = "name";
-        public static final String COLUMN_LAB = "lab";
-        public static final String COLUMN_MAIL = "mail";
-        public static final String COLUMN_PHONE = "phone";
-        public static final String COLUMN_SUBJECT_ID = "subject_id";
+        public static final String COL_ID = "id";
+        public static final String COL_NAME = "name";
+        public static final String COL_LAB = "lab";
+        public static final String COL_MAIL = "mail";
+        public static final String COL_PHONE = "phone";
+        public static final String COL_SUBJECT_ID = "subject_id";
     }
 
     public static class EnrollingInfoEntity {
@@ -60,32 +61,45 @@ public class DBContract {
         // Use this as the ID of a unspecified subject
         public static final int NULL_ID = 0;
         public static final String TABLE_NAME = "enrolling_info";
-        public static final String COLUMN_ID = "id";
-        public static final String COLUMN_DAY_OF_WEEK = "day_of_week";
-        public static final String COLUMN_PERIOD = "period";
-        public static final String COLUMN_SUBJECT_ID = "subject_id";
+        public static final String COL_ID = "id";
+        public static final String COL_DAY_OF_WEEK = "day_of_week";
+        public static final String COL_PERIOD = "period";
+        public static final String COL_SUBJECT_ID = "subject_id";
     }
 
     public static class LocationEntity {
         private LocationEntity() {}
 
         public static final String TABLE_NAME = "location";
-        public static final String COLUMN_ID = "id";
-        public static final String COLUMN_NAME = "name";
+        public static final String COL_ID = "id";
+        public static final String COL_NAME = "name";
     }
 
     public static class AssignmentEntity {
         private AssignmentEntity() {}
 
         public static final String TABLE_NAME = "assignment";
-        public static final String COLUMN_ID = "id";
-        public static final String COLUMN_TITLE = "title";
-        public static final String COLUMN_ENROLLING_INFO_ID = "enrolling_info_id";
-        public static final String COLUMN_NOTE = "note";
-        public static final String COLUMN_IS_DONE = "is_done";
-        public static final String COLUMN_DEADLINE_YEAR = "deadline_y";
-        public static final String COLUMN_DEADLINE_MONTH = "deadline_m";
-        public static final String COLUMN_DEADLINE_DAY = "deadline_d";
-        public static final String COLUMN_DEADLINE_DAY_OF_WEEK = "deadline_d_of_w";
+        public static final String COL_ID = "id";
+        public static final String COL_TITLE = "title";
+        public static final String COL_ENROLLING_INFO_ID = "enrolling_info_id";
+        public static final String COL_NOTE = "note";
+        public static final String COL_IS_DONE = "is_done";
+        public static final String COL_DEADLINE_YEAR = "deadline_y";
+        public static final String COL_DEADLINE_MONTH = "deadline_m";
+        public static final String COL_DEADLINE_DAY = "deadline_d";
+        public static final String COL_DEADLINE_DAY_OF_WEEK = "deadline_d_of_w";
+    }
+
+    public static class NotificationEntity {
+        private NotificationEntity() {}
+
+        public static final String TABLE_NAME = "notification";
+        public static final String COL_ID = "id";
+        public static final String COL_TITLE = "title";
+        public static final String COL_NOTE = "note";
+        public static final String COL_SUBJECT_ID = "subject_id";
+        public static final String COL_CATEGORY_ID = "category_type";
+        public static final String COL_DATETIME_START = "datetime_start";
+        public static final String COL_DATETIME_END = "datetime_end";
     }
 }
---
+++ app/src/main/java/com/d/candy/f/awesometimetable/structure/Notification.java
@@ -0,0 +1,27 @@
+package com.d.candy.f.awesometimetable.structure;
+
+import android.content.ContentValues;
+
+/**
+ * Created by daichi on 7/21/17.
+ */
+
+public class Notification extends Entity {
+
+    public Notification() {}
+
+    @Override
+    public EntityType getEntityType() {
+        return null;
+    }
+
+    @Override
+    public String getAffiliation() {
+        return null;
+    }
+
+    @Override
+    public ContentValues toContentValues() {
+        return null;
+    }
+}
